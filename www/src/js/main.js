bf47368208f195f1f0dc501226942615e68799b2
Matej Zelenka <Matej.Zel@seznam.cz>
2022-11-07 13:47:19 +0100

    index.html responsive innit, WIP mobile menu

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+function burgerMenuToggle() {
+  const burger = document.querySelector('.burger')
+  const mobileMenu = document.querySelector('.mobile-menu')
+  const ul = document.querySelector('.navigation')
+
+  burger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('shown')
+    ul.classList.toggle('mb-0')
+    burger.classList.toggle('open')
+  })
+}
+
+burgerMenuToggle()
